c2a889c24f0c6b0fff9edb6c1e5b9ddec3c7ce12
Nastya <nasticbazlic@gmail.com>
2021-02-09 23:44:32 -0900

    swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
-
+new Swiper ('.about__slider', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+});
